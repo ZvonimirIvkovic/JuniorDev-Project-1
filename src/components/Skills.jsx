4c510e0637233f39b7a41c32a0fa73f9d596e62d
Zvonimir Ivković <145219655+ZvonimirIvkovic@users.noreply.github.com>
2024-03-03 18:12:06 +0100

    Fixing issues in Skill.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,15 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import '../assets/styles/Card.css'
 import '../assets/styles/Skills.css'
 
-function Skill({skill, percentile, index}){
+function Skill({skill, percentile}){
+
     return <div className="skill">
                 <p>{skill}</p>
-                <div class="loading-bar">
-                    <div class="progress-bar">
-                        <p className="percentiles">{
-                            document.getElementsByClassName('progress-bar')[index].style.width = percentile
-                        }</p>
+                <div className="loading-bar">
+                    <div className="progress-bar" style={{ width: `${percentile}` }}>
+                        <p className="percentiles">{percentile}</p>
                     </div>
                 </div>
             </div>
